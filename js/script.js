11d214dd0f0c3539f9621438c8941f1e5f6623c6
MuhammadRumii <muhammadrumii28@gmail.com>
2024-08-29 17:45:34 +0700

    penambahan banner auto slide

--- js/script.js
+++ js/script.js
@@ -10,6 +10,27 @@ document.getElementById('submitName').addEventListener('click', function() {
     }
 });
 
+// image slideshow
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var imgList = document.getElementsByClassName("img-slideshow");
+    if (n > imgList.length) slideIndex = 1;
+    if (n < 1) slideIndex = imgList.length;
+
+    for(i = 0; i < imgList.length; i++) {
+        imgList[i].style.display = "none";
+    }
+
+    imgList[slideIndex - 1].style.display = "block";
+}
+
 
 // form output
 document.getElementById('contactForm').addEventListener('submit', function(event) {
